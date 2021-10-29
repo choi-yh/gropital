661b43b39927315356a77a0fa59a48c605b10a22
younghyo_desktop <yhchoi0102@gmail.com>
2021-10-29 21:42:42 +0900

    google authentication

--- simpoon/urls.py
+++ simpoon/urls.py
@@ -5,6 +5,13 @@ from . import views
 app_name = "simpoon"
 
 urlpatterns = [
-    path("", views.index), 
-    # path("login/", views.login)
+    path("", views.index),
+    
+    path("google/login/", views.google_login, name="google_login"),
+    path("google/callback/", views.google_callback, name="google_callback"),
+    path(
+        "google/login/finish/",
+        views.GoogleLogin.as_view(),
+        name="google_login_todjango",
+    ),
 ]
