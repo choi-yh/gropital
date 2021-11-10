585a4caf17b73b61448a445854171252ca7a73a3
YoungHyo <yhchoi0102@gmail.com>
2021-11-11 01:40:22 +0900

    네이버 로그인 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import google_views, kakao_views
+from .views import google_views, kakao_views, naver_views
 
 urlpatterns = [
+    # Google url
     path("google/login/", google_views.google_login, name="google_login"),
     path("google/callback/", google_views.google_callback, name="google_callback"),
     path(
@@ -11,11 +12,21 @@ urlpatterns = [
         name="google_login_to_django",
     ),
 
+    # Kakao url
     path("kakao/login/", kakao_views.kakao_login, name="kakao_login"),
     path("kakao/callback/", kakao_views.kakao_callback, name="kakao_callback"),
     path(
         "kakao/login/finish/",
         kakao_views.KakaoLogin.as_view(),
         name="kakao_login_to_django"
+    ),
+
+    # Naver url
+    path("naver/login/", naver_views.naver_login, name="naver_login"),
+    path("naver/callback/", naver_views.naver_callback, name="naver_callback"),
+    path(
+        "naver/login/finish/",
+        naver_views.NaverLogin.as_view(),
+        name="naver_login_to_django",
     )
 ]
